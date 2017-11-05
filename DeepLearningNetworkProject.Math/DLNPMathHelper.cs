cc107af90363f81273bf95848021cfa243302cba
Sebastian Flosbach <sebastian.flosbach@gmail.com>
2017-11-05 20:00:15 +0100

    - Added Sigmoid

---
+++ DeepLearningNetworkProject.Math/DLNPMathHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLNP.Math
+{
+    public static class DLNPMathHelper
+    {
+
+        /// <summary>
+        /// Calculates the sigmoid function for a given value
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns>Value between 0 and 1 depending on the input</returns>
+        public static double Sigmoid(double x)
+        {
+            return 1 / (1 + System.Math.Exp(x));
+        }
+    }
+}
