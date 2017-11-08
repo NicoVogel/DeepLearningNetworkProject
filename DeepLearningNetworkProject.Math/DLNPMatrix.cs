6fe1e517ab32ea8fac6b4880e58bdbefae65e01c
Sebastian Flosbach <sebastian.flosbach@gmail.com>
2017-11-08 14:41:48 +0100

    - Basic implementation of DLNPMatrix

--- DeepLearningNetworkProject.Math/DLNPMatrix.cs
+++ DeepLearningNetworkProject.Math/DLNPMatrix.cs
@@ -1,4 +1,5 @@
 ﻿using DLNP.Entities.Interfaces.Business.Models;
+using System;
 
 namespace DLNP.Math
 {
@@ -25,20 +26,69 @@ namespace DLNP.Math
             }
         }
 
+        public int Rows { get => m_components.GetLength(0); }
+
+        public int Columns { get => m_components.GetLength(1); }
+
         #endregion
 
         #region Constructor
 
-        public DLNPMatrix()
+        public DLNPMatrix(int rows, int columns)
         {
-
+            m_components = new double[rows, columns];
         }
 
         #endregion
 
         #region Operators
 
+        public static DLNPMatrix operator *(DLNPMatrix a, DLNPMatrix b)
+        {
+            if(a.Columns == b.Rows && a.Rows == b.Columns)
+            {
+                DLNPMatrix result = new DLNPMatrix(a.Rows, b.Columns);
 
+                for(int m = 0; m < a.Rows; m++)
+                {
+                    for(int n = 0; n < b.Columns; n++)
+                    {
+                        for(int i = 0; i < a.Columns; i++)
+                        {
+                            result[m, n] += a[m, i] * b[i, n];
+                        }
+                    }
+                }
+
+                return result;
+            }
+            else
+            {
+                throw new InvalidOperationException("Matrices are not compatible!");
+            }
+        }
+
+        public static DLNPVector operator *(DLNPMatrix a, DLNPVector b)
+        {
+            if(a.Columns == b.Size)
+            {
+                DLNPVector result = new DLNPVector(b.Size);
+
+                for(int m = 0; m < b.Size; m++)
+                {
+                    for(int i = 0; i < b.Size; i++)
+                    {
+                        result[m] += a[m, i] * b[i];
+                    }
+                }
+
+                return result;
+            }
+            else
+            {
+                throw new InvalidOperationException("Matrix and vector size are not compatible!");
+            }
+        }
 
         #endregion
     }
