f21714538ffd5b87fd4527ecc68fb2defe7628aa
Sebastian Flosbach <sebastian.flosbach@gmail.com>
2017-11-17 21:51:20 +0100

    - Implemented MathFactory

---
+++ DeepLearningNetworkProject.Entities/Interfaces/Factories/IMathFactory.cs
@@ -0,0 +1,15 @@
+﻿
+using DLNP.Entities.Interfaces.Business.Models;
+
+namespace DLNP.Entities.Interfaces.Factories
+{
+    public interface IMathFactory
+    {
+
+        IVector CreateVector(int size);
+
+        IVector CreateVector(params double[] args);
+
+        IMatrix CreateMatrix(int rows, int columns);
+    }
+}
---
+++ DeepLearningNetworkProject.Math/Factory/MathFactory.cs
@@ -0,0 +1,36 @@
+﻿using DLNP.Entities.Interfaces.Business.Models;
+using DLNP.Entities.Interfaces.Factories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DLNP.Math.Factory
+{
+    public class MathFactory : IMathFactory
+    {
+        #region Constructor
+
+        public MathFactory()
+        {
+
+        }
+
+        #endregion
+
+        public IVector CreateVector(int size)
+        {
+            return new DLNPVector(size);
+        }
+
+        public IVector CreateVector(params double[] args)
+        {
+            return new DLNPVector(args);
+        }
+
+        public IMatrix CreateMatrix(int rows, int columns)
+        {
+            return new DLNPMatrix(rows, columns);
+        }
+
+    }
+}
