3dd3625e63ebbf6c521be21f23d6da0342f0f05c
Sebastian Flosbach <sebastian.flosbach@gmail.com>
2017-11-19 20:19:46 +0100

    -  Experimenting with math

---
+++ DeepLearningNetworkProject.Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using DLNP.Entities.Interfaces.Business;
+using DLNP.Entities.Interfaces.Factories;
+using DLNP.Factory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepLearningNetworkProject.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PresentationFactory pf = new PresentationFactory();
+            IProgramController pc = pf.CreateProgramController();
+            IEntityFactory ef = pf.EntityFactory;
+
+            IList<int> layers = ef.CreateList<int>();
+            layers.Add(2);
+            layers.Add(3);
+            layers.Add(2);
+
+            pc.InizialiseNetwork(layers);
+        }
+    }
+}
---
+++ DeepLearningNetworkProject.Console/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DeepLearningNetworkProject.Console")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DeepLearningNetworkProject.Console")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("16b8904a-1c2f-46d3-8b2a-671b934d3b1f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
